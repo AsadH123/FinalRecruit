aa55126ab164427cf703674b42b97d2b627fcd8c
AsadH123 <asadh821@gmail.com>
2024-08-29 15:15:05 +0500

    adding space

--- src/test/java/Candidate_Inner_Action_List/Active_Change_Status.java
+++ src/test/java/Candidate_Inner_Action_List/Active_Change_Status.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.WebDriverWait; 
 
 public class Active_Change_Status {
 
